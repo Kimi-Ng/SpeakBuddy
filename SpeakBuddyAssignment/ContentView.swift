01b5e1ec61bc9b57c9738c58c74402b0349688ff
Kimi <z.b.kimi@gmail.com>
2023-10-10 15:11:21 +0900

    Project init

---
+++ SpeakBuddyAssignment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpeakBuddyAssignment
+//
+//  Created by Chengyuan Wu on 2023/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpeakBuddyAssignment/SpeakBuddyAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakBuddyAssignmentApp.swift
+//  SpeakBuddyAssignment
+//
+//  Created by Chengyuan Wu on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct SpeakBuddyAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
